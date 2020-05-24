ddeb4597803f45f1d7f7f713af1b5718fa02fe00
Danilo Martins <31545760+DaniMarts@users.noreply.github.com>
2020-05-24 18:33:08 +0200

    React routes partially working

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {BrowserRouter, Route, withRouter} from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import ProfilePage from './components/ProfilesPage';
 import './App.css';
@@ -17,48 +18,80 @@ class App extends Component {
 
   render(){
     return (
-      <div className="App">
-        {this.state.isLoggedIn ? 
-          <ProfilePage
-            developers={this.state.developers} 
-          /> 
-          : 
-          <LoginPage 
-            login={this.login}
-          />
-        }
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          {/* Creating routes: one for the login page, and one for the profiles page */}
+          
+            <Route exact={true} path="/" render={(props) => <LoginPage {...props} login={this.login}/>}/>
+            
+            <Route path="/profiles" render={(props)=>
+              <ProfilePage 
+                {...props}
+                developers={this.state.developers} 
+                viewProfilePage={this.viewProfile}
+              />}
+            />
+          
+
+          {/* {this.state.isLoggedIn ? 
+            <ProfilePage
+              developers={this.state.developers} 
+              viewProfilePage={this.viewProfile}
+            /> 
+            : 
+            <LoginPage 
+              login={this.login}
+            />
+          } */}
+        </div>
+      </BrowserRouter>
     );
   }
 
   login = (loginDetails)=>{
-    // sending the data to the server
+    // sending the data to the server, to check if the login details are valid
     fetch("/api/auth", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(loginDetails)
     }) 
-    .then(res =>  res.json()) //only work if there's a response
+    .then(res =>  res.json()) // only works if there's a response
     .then(json => {
         if(json.username){ // if the username was found
             if (json.successfullLogin){ // if the login was successfull
-                // fetch the developer profiles
+                this.setState({isLoggedIn: true, username: json.username}); // the admin has successfully logged in
+                
+                // fetching the developer profiles
                 fetch("/api/profiles").then(data=>data.json())
-                    .then(json=>{this.setState({developers: json});
-                    console.log(this.state);
-            });
+                // adding the profiles to the state of the app
+                    .then(json=>{
+                      this.setState({developers: json});
+                      console.log(this.state); // REMOVE THIS LINE
+                      this.props.history.push("/profiles"); // redirecting the admin to the profiles page
+                      console.log(this.props);// REMOVE THIS LINE
+                    })
+                    .catch(err => console.log("Error: ", err));
             }
             else{ // the password was wrong
                 console.log("Wrong password");
             }
         }
-        else{
+        else{ // the username does not exits, or is not an admin
             console.log("Username not found");
         }
     })
     .catch(err => console.log("Error: ", err));        
   }
+
+  // this function is passed on to the profile items, so that the single profile can be viewed on a separate page
+  viewProfile = (profile) => {
+    let username = profile.name;
+    let id = profile.id;
+
+    console.log(`You want to view the profile of ${username} with id of ${id}`);
+  }
 }
+
   
 
-export default App;
+export default withRouter(App); // withRouter is here so we can use props.history.push, to redirect the user
--- client/src/components/ProfilesPage.js
+++ client/src/components/ProfilesPage.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 
 export default function ProfilesPage(props){
-    return (
-        <div className="Profiles">
-            
+    // this variable holds the JSX for each profile
+    // each element is a link to the developer's profile page
+    // when this element is clicked, it'll forward the username and id of the developer, to the main app
+    var ProfileItems = props.developers.map(developer => (
+            <li  onClick={() => props.viewProfilePage({name: developer.username, id: developer._id})} key={developer._id}>
+                <NavLink to={`/${developer.username}/${developer._id}`}>
+                    <span className="name">{developer.username}</span>
+                    {/* <span className="rank">{developer.stars}</span> */}
+                </NavLink>      
+            </li>
+        )    
+    );
 
+    return (
+        <div className="profiles-container">
+            <ul className="profile-list">
+                {ProfileItems}
+            </ul>
+            
         </div>
     );
 }
